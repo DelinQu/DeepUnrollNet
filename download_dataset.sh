b6b5fc6fb53373e7919e62212ba66ccfe76aa948
Delin Qu <qdl.cs@qq.com>
2022-02-07 17:13:13 +0800

    Create download_dataset.sh

---
+++ download_dataset.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# download Carla and unzip 
+gdown --id 1rvN0q5KF9BTxBOx4oOyxJKzX8XkUB-zM
+unzip carla_rs_test.zip
+mv test Carla_rs_test
+
+# download Fastec and unzip
+gdown --id 1Yfu6sOgIa6vJ6VwAedycjLu-_7rx7vZ2
+unzip fastec_rs_test.zip
+mv test Fastec_rs_test
