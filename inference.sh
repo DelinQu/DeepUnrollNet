dc069db595dc544bb90f57058d808ee0727669a1
Delin Qu <qdl.cs@qq.com>
2022-02-07 17:18:50 +0800

    Update inference.sh

--- inference.sh
+++ inference.sh
@@ -2,17 +2,24 @@
 
 # !! Updata the path to the dataset and directory to 
 # !! save your trained models with your own local path !!
+# dataset
 carla_dataset_type=Carla
-carla_root_path_test_data=path_to_carla_rs/test
+carla_root_path_test_data=../Carla_rs_test/
 
 fastec_dataset_type=Fastec
-fastec_root_path_test_data=path_to_fastec_rs/test/
+fastec_root_path_test_data=../Fastec_rs_test/
 
+# model
 model_dir=../experiments/pretrained_models/
 
-results_dir=path_to_results_dir/
+# result
+mkdir -p experiments/Carla
+mkdir -p experiments/Fastec
 
-#
+results_dir_Carla=../experiments/Carla/
+results_dir_Fastec=../experiments/Fastec/
+
+# inference
 cd deep_unroll_net
 
 python inference.py \
@@ -20,7 +27,7 @@ python inference.py \
           --dataset_root_dir=$carla_root_path_test_data \
           --log_dir=$model_dir \
           --net_type='netMiddle' \
-          --results_dir=$results_dir \
+          --results_dir=$results_dir_Carla \
           --model_label=pretrained_carla \
           --compute_metrics 
 
@@ -30,6 +37,6 @@ python inference.py \
           --log_dir=$model_dir \
           --net_type='netMiddle' \
           --results_dir=$results_dir \
-          --model_label=pretrained_fastec \
+          --model_label=results_dir_Fastec \
           --compute_metrics 
           
