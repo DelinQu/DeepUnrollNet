de479ba348041cb9798e23e70c9727d3bfcf03f3
Delin Qu <qdl.cs@qq.com>
2022-02-08 16:59:33 +0800

    Update inference_drsc_metrics.sh

--- inference_drsc_metrics.sh
+++ inference_drsc_metrics.sh
@@ -2,7 +2,8 @@
 
 # create an empty folder for experimental results
 # mkdir -p experiments/Carla
-mkdir -p experiments/Fastec
+# mkdir -p experiments/Fastec
+mkdir -p experiments/IMU_test
 
 cd deep_unroll_net
 
@@ -14,9 +15,16 @@ cd deep_unroll_net
 #             --results_dir=../experiments/Carla \
 #             --data_dir='../Carla_rs_test/Carla_town05'
 
+# python inference_drsc_metrics.py \
+#             --model_label='pretrained_fastec' \
+#             --log_dir=../experiments/pretrained_models \
+#             --net_type='netMiddle' \
+#             --results_dir=../experiments/Fastec \
+#             --data_dir='../Fastec_rs_test/'
+
 python inference_drsc_metrics.py \
             --model_label='pretrained_fastec' \
             --log_dir=../experiments/pretrained_models \
             --net_type='netMiddle' \
-            --results_dir=../experiments/Fastec \
-            --data_dir='../Fastec_rs_test/'
+            --results_dir=../experiments/IMU_test \
+            --data_dir='../input/imu-test/IMU_test'
